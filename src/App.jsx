9b0e8122a3359251feaf7a6b122ffa0621effe10
Antonyamose <antonyamose3@gmail.com>
2026-06-09 10:24:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Components/Hero'
+import About from './Components/About'
+import Contact from './Components/Contact'
+
+function App() {
+  return (
+    <div>
+      <Hero/>
+      <About/>
+      <Contact/>
+    </div>
+  )
+}
+
+export default App
